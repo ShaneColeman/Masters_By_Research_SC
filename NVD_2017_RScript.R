b2623f57275a233ecd5e5d62f5aeb3494de41a37
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-21 17:59:39 +0100

    Packages installed and imported

--- NVD_2017_RScript.R
+++ NVD_2017_RScript.R
@@ -1,2 +1,14 @@
 #NVD_2017_RScript.R
 
+install.packages("XML")
+install.packages("methods")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("anytime")
+
+library(XML)
+library(methods)
+library(plyr)
+library(dplyr)
+library(anytime)
+

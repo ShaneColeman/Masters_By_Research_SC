2a9d7d04430b00fa836b9441d9c94539b0873d64
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-21 17:49:02 +0100

    URLs added to NVD_Test_RScript.R

--- NVD_Test_RScript.R
+++ NVD_Test_RScript.R
@@ -1,4 +1,17 @@
-#NVD_2016_RScript.R
+#NVD_Test_RScript.R
+
+#URLs
+#https://www.tutorialspoint.com/r/r_xml_files.htm
+#https://cran.r-project.org/web/packages/XML/XML.pdf
+#https://stackoverflow.com/questions/4227223/r-list-to-data-frame
+#https://stackoverflow.com/questions/27594541/export-a-list-into-a-csv-or-txt-file-in-r
+#https://stackoverflow.com/questions/5746599/r-dataframe-naming-the-headers
+#http://rpubs.com/jsmanij/131030
+#http://www.statmethods.net/management/merging.html
+#https://stackoverflow.com/questions/10358680/adding-a-one-dataframe-to-the-the-end-of-another-data-frame-in-r
+#http://www.sthda.com/english/wiki/identifying-and-removing-duplicate-data-in-r
+#https://stackoverflow.com/questions/13456241/convert-unix-epoch-to-date-object-in-r
+#http://rprogramming.net/write-csv-in-r/
 
 install.packages("XML")
 library(XML)

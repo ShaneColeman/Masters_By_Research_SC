6e3ff90ac181038dbec62816f6da8d6bfff77249
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-22 14:17:45 +0100

    Installation packages added and the library() function called on the packages. NVD CVE XML file added and stored as variable nvd2016. XML data converted to a list through the xmlParse() function and stored as nvd2016List. List stored as a data frame called dataNVD2016. CVE ID attribute extracted from dataNVD2016 data frame, and stored in variable cveID2016. Data frame created to store CVE IDs, called cveID2016DataFrame. Column name "CVE_ID" added to data frame cveID2016DataFrame.

--- NVD_2016_RScript.R
+++ NVD_2016_RScript.R
@@ -1,1 +1,25 @@
 #NVD_2016_RScript.R
+
+install.packages("XML")
+install.packages("methods")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("anytime")
+
+library(XML)
+library(methods)
+library(plyr)
+library(dplyr)
+library(anytime)
+
+nvd2016 <- xmlParse(file = "nvdcve-2.0-2016.xml")
+summary(nvd2016)
+
+nvd2016List <- xmlToList(nvd2016)
+nvd2016List
+dataNVD2016 <- ldply(.data = nvd2016List, data.frame)
+
+cveID2016 <- dataNVD2016$cve.id
+cveID2016
+cveID2016DataFrame <- ldply(cveID2016, data.frame)
+colnames(cveID2017DataFrame) <- "CVE_ID"

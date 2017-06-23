86d5896a293b04da9a99b3beff09a725c3ccf58d
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-23 10:24:13 +0100

    CVE ID attribute extracted from dataNVD2014 data frame, and stored in variable cveID2014. Data frame created to store CVE IDs, called cveID2014DataFrame. Column name "CVE_ID" added to data frame cveID2014DataFrame.

--- NVD_2014_RScript.R
+++ NVD_2014_RScript.R
@@ -18,3 +18,8 @@ summary(nvd2014)
 nvd2014List <- xmlToList(nvd2014)
 nvd2014List
 dataNVD2014 <- ldply(.data = nvd2014List, data.frame)
+
+cveID2014 <- dataNVD2014$cve.id
+cveID2014
+cveID2014DataFrame <- ldply(cveID2014, data.frame)
+colnames(cveID2014DataFrame) <- "CVE_ID"

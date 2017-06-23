a2c4e2090d433d2d5f711ec7c1b2d34555c45afd
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-23 10:30:24 +0100

    Summary attribute extracted from dataNVD2014 data frame, and stored in variable summary2014. Data frame created to store Summaries, called summary2014DataFrame. Column name "Vulnerability_Summary" added to data frame summary2014DataFrame.

--- NVD_2014_RScript.R
+++ NVD_2014_RScript.R
@@ -23,3 +23,8 @@ cveID2014 <- dataNVD2014$cve.id
 cveID2014
 cveID2014DataFrame <- ldply(cveID2014, data.frame)
 colnames(cveID2014DataFrame) <- "CVE_ID"
+
+summary2014 <- dataNVD2014$summary
+summary2014
+summary2014DataFrame <- ldply(summary2014, data.frame)
+colnames(summary2014DataFrame) <- "Vulnerability_Summary"

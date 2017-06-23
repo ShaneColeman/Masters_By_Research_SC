dbf9517aa31bddee05da130208954dada9179a7d
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-23 11:38:04 +0100

    Packages installed through the install.packages() function and imported via the library() function.

--- NVD_2013_RScript.R
+++ NVD_2013_RScript.R
@@ -1,1 +1,13 @@
 #NVD_2013_RScript.R
+
+install.packages("XML")
+install.packages("methods")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("anytime")
+
+library(XML)
+library(methods)
+library(plyr)
+library(dplyr)
+library(anytime)

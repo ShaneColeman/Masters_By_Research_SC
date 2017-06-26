2d803253196beb08f3c78de0b079d0dc572cd06e
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-06-26 19:34:05 +0100

    Code alterations made to NVD_2013_RScript.R

--- NVD_2013_RScript.R
+++ NVD_2013_RScript.R
@@ -6,6 +6,9 @@ install.packages("plyr")
 install.packages("dplyr")
 install.packages("anytime")
 
+#install.packages("xml2")
+#install.packages("purrr")
+#install.packages("broom")
 #install.packages("stringi")
 #install.packages("data.table")
 #install.packages("reshape2")
@@ -16,8 +19,11 @@ library(plyr)
 library(dplyr)
 library(anytime)
 
-library(stringi)
-#library(data.table)
+#library(xml2)
+#library(purrr)
+#library(broom)
+#library(stringi)
+library(data.table)
 #library(reshape2)
 
 nvd2013 <- xmlParse(file = "nvdcve-2.0-2013.xml")
@@ -25,9 +31,33 @@ summary(nvd2013)
 
 nvd2013List <- xmlToList(nvd2013)
 nvd2013List
-dataNVD2013 <- ldply(.data = nvd2013List, data.frame)
+#dataNVD2013 <- ldply(.data = nvd2013List, data.frame)
+dataNVD2013[1: length(nvd2013List$entry)] <- as.vector(nvd2013List$entry$"cve-id")
+dataNVD2013
 
-cveID2013 <- dataNVD2013$.id
-cveID2013
-cveID2014DataFrame <- ldply(cveID2014, data.frame)
-colnames(cveID2014DataFrame) <- "CVE_ID"
+colnames(dataNVD2013) <- "CVE_ID"
+#----------------------------------------------------------------------------------
+#https://stackoverflow.com/questions/5424859/parsing-xml-to-list-in-r-how-to-consistently-access-nodes-when-xml-structure-va
+
+xmlNVD2013 <- xmlTreeParse(file = "nvdcve-2.0-2013.xml", useInternalNodes = TRUE)
+
+cveID2013 <- xpathApply(xmlNVD2013, "//vuln:cve-id", xmlValue)
+cveID2013List <- as.list(cveID2013)
+cveID2013DataFrame <- as.data.table(cveID2013List)
+cveID2013DataFrameStack <- stack(cveID2013DataFrame)
+colnames(cveID2013DataFrameStack) <- "CVE_ID"
+rm(cveID2013DataFrame)
+
+cvss2013 <- xpathApply(xmlNVD2013, "//cvss:score", xmlValue)
+cvss2013List <- as.list(cvss2013)
+cvss2013DataFrame <- as.data.table(cvss2013List)
+cvss2013DataFrame[cvss2013DataFrame == 'NA'] <- "NA"
+cvss2013DataFrameStack <- stack(cvss2013DataFrame)
+#----------------------------------------------------------------------------------
+
+#nvd2013List <- xmlToList(nvd2013)
+#nvd2013List
+#df <- data.frame(nvd2013List$entry$'cve-id'[[1 : length(nvd2013List)])
+#dataNVD2013 <- ldply(.data = nvd2013List, data.frame)
+#dataNVD2013 <- ldply(xmlToList("nvdcve-2.0-2013.xml"), data.frame)
+

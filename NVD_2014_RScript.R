743740026f625082a24a471439b4701bf6415740
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-04 15:23:20 +0100

    publishedTime2014DataFrame Data Frame added to nvd2014Total Data Frame. nvd2014TotalDistinctRejectRemoved Data Frame created to remove 'DO NOT USE THIS CANDIDATE NUMBER.' entries.

--- NVD_2014_RScript.R
+++ NVD_2014_RScript.R
@@ -102,11 +102,14 @@ cwe2014DataFrame <- ldply(cwe2014, data.frame)
 colnames(cwe2014DataFrame) <- "CWE"
 
 nvd2014Total <- cbind(cveID2014DataFrame, summary2014DataFrame, 
-											publishedDate2014DataFrame, lastModifiedDate2014DataFrame, 
+											publishedDate2014DataFrame, publishedTime2014DataFrame, lastModifiedDate2014DataFrame, 
 											cvss2014DataFrame, cvssAccessVector2014DataFrame, cvssAccessComplexity2014DataFrame, 
 											cvssAuthentication2014DataFrame, cvssConfidentialityImpact2014DataFrame, 
 											cvssIntegrityImpact2014DataFrame, cvssAvailabilityImpact2014DataFrame, 
 											cvssGeneratedOnDate2014DataFrame, cwe2014DataFrame)
 nvd2014TotalDistinct <-distinct(nvd2014Total)
+
+#http://www.duanqu.tech/questions/4900348/how-to-remove-rows-in-a-dataframe-that-contain-certain-words-in-r
+nvd2014TotalDistinctRejectRemoved <- nvd2014TotalDistinct[!grepl("DO NOT USE THIS CANDIDATE NUMBER.", nvd2014TotalDistinct$Vulnerability_Summary), ]
 
 write.csv(nvd2014TotalDistinct, "CVE_2014.csv")

d4512121955cc4b25f0fe93dc689152ffcc22047
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-12 17:44:15 +0100

    NVD_Training_RScript.R renamed to NVD_Training_Testing_RScript.R. Bracket identifers [] (relating to NVD_Steps_Text_File) modified in NVD_Training_Testing_RScript.R, relating to diverse pre-processing sections within this RScript.

--- NVD_Training_Testing_RScript.R
+++ NVD_Training_Testing_RScript.R
@@ -1,17 +1,18 @@
-#NVD_Training_RScript.R
+#NVD_Training_Testing_RScript.R
 
 #----------Training----------#
-#[30]
+#[31]
 #https://stackoverflow.com/questions/37257572/how-can-i-combine-two-dataframes-with-different-lengths-in-r
 nvdMerge2014_2015 <- merge(nvd2014TotalDistinctNARemoved, nvd2015TotalDistinctNARemoved, all = TRUE)
 
-#[31]
+#[32]
 nvdMerge2014_2015_2016 <- merge(nvdMerge2014_2015, nvd2016TotalDistinctNARemoved, all = TRUE)
 
-#[32]
+#[33]
 nvdTraining <- nvdMerge2014_2015_2016
 write.csv(nvdTraining, "NVD_Training.csv", row.names = FALSE)
 
 #----------Testing----------#
+#[34]
 nvdTesting <- nvd2017TotalDistinctNARemoved
 write.csv(nvdTesting, "NVD_Testing.csv", row.names = FALSE)

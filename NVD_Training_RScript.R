84e59c657110d38f391b8499256e79588a258683
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-12 16:44:11 +0100

    NVD_Steps_Text_File modified with new content. nvd2014TotalDistinctNARemoved data frame and nvd2015TotalDistinctNARemoved data frame merged, by use of the merge() function. Data frame nvdMerge2014_2015 was created, which combines both data frames: nvd2014TotalDistinctNARemoved and nvd2015TotalDistinctNARemoved. nvdMerge2014_2015 data frame and nvd2016TotalDistinctNARemoved data frame merged, by use of the merge() function. Data frame nvdMerge2014_2015_2016 was created, which combines both data frames: nvd2014TotalDistinctNARemoved and nvd2015TotalDistinctNARemoved. nvdTraining data frame created, which calls upon the newly created data frame: nvdMerge2014_2015_2016. write.csv() function used on nvdTraining data frame to create CSV file called "NVD_Training.csv". NVD_Training.csv file saved in RStudio project workspace. nvdTesting data frame created, which calls upon the data frame: nvd2017TotalDistinctNARemoved. write.csv() function used on nvdTesting data frame to create CSV file called "NVD_Testing.csv". NVD_Testing.csv file saved in RStudio project workspace.

--- NVD_Training_RScript.R
+++ NVD_Training_RScript.R
@@ -1,1 +1,17 @@
 #NVD_Training_RScript.R
+
+#----------Training----------#
+#[30]
+#https://stackoverflow.com/questions/37257572/how-can-i-combine-two-dataframes-with-different-lengths-in-r
+nvdMerge2014_2015 <- merge(nvd2014TotalDistinctNARemoved, nvd2015TotalDistinctNARemoved, all = TRUE)
+
+#[31]
+nvdMerge2014_2015_2016 <- merge(nvdMerge2014_2015, nvd2016TotalDistinctNARemoved, all = TRUE)
+
+#[32]
+nvdTraining <- nvdMerge2014_2015_2016
+write.csv(nvdTraining, "NVD_Training.csv", row.names = FALSE)
+
+#----------Testing----------#
+nvdTesting <- nvd2017TotalDistinctNARemoved
+write.csv(nvdTesting, "NVD_Testing.csv", row.names = FALSE)

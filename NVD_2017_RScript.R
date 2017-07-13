65aa1b3d9f7bfe5aa220390d778e13a5faec0a70
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-13 11:56:03 +0100

    nvd2017TotalDistinctReject data frame created, which comprises of all the values where the text "DO NOT USE THIS CANDIDATE NUMBER" within the Vulnerability_Summary appears.
    nvd2017TotalDistinctNA data frame created, which comprises of all the CVSS_Scores which are NA.

--- NVD_2017_RScript.R
+++ NVD_2017_RScript.R
@@ -387,10 +387,12 @@ write.csv(nvd2017TotalDistinctLastRowRemoved, "CVE_2017_Last_Row_Removed.csv", r
 
 #[29]
 #http://www.duanqu.tech/questions/4900348/how-to-remove-rows-in-a-dataframe-that-contain-certain-words-in-r
+nvd2017TotalDistinctReject <- nvd2017TotalDistinctLastRowRemoved[grepl("DO NOT USE THIS CANDIDATE NUMBER.", nvd2017TotalDistinctLastRowRemoved$Vulnerability_Summary), ]
 nvd2017TotalDistinctRejectRemoved <- nvd2017TotalDistinctLastRowRemoved[!grepl("DO NOT USE THIS CANDIDATE NUMBER.", nvd2017TotalDistinctLastRowRemoved$Vulnerability_Summary), ]
 write.csv(nvd2017TotalDistinctRejectRemoved, "CVE_2017_Rejected_Removed.csv", row.names = FALSE)
 
 #[30]
 #https://stackoverflow.com/questions/20977972/how-to-remove-na-data-in-only-one-columns
+nvd2017TotalDistinctNA <- nvd2017TotalDistinctRejectRemoved[is.na(nvd2017TotalDistinctRejectRemoved$CVSS_Score),]
 nvd2017TotalDistinctNARemoved <- nvd2017TotalDistinctRejectRemoved[!is.na(nvd2017TotalDistinctRejectRemoved$CVSS_Score),]
 write.csv(nvd2017TotalDistinctNARemoved, "CVE_2017_NA_Removed.csv", row.names = FALSE)

dd85a7c86b0a68e3de4e22820d59aaf1227630be
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-25 11:05:39 +0100

    XML converted into a list and stored in variable nvd2017UpdatedList, then list was converted into a data frame and stored in dataNVD2017Updated.

--- NVD_2017_Updated_RScript.R
+++ NVD_2017_Updated_RScript.R
@@ -17,3 +17,8 @@ library(anytime)
 #[3]
 nvd2017Updated <- xmlParse(file = "nvdcve-2.0-2017-updated.xml")
 summary(nvd2017Updated)
+
+#[4]
+nvd2017UpdatedList <- xmlToList(nvd2017Updated)
+nvd2017UpdatedList
+dataNVD2017Updated <- ldply(.data = nvd2017UpdatedList, data.frame)

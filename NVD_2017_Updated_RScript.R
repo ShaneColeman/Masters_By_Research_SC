f7fc3c80d93700d857f8ac1d4ffe56aed9e56471
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-25 10:53:18 +0100

    Packages installed through the install.packages() function and imported via the library() function. "XML" "methods" "plyr" "dplyr" "anytime"
    nvdcve-2.0-2017-updated.xml copied / added into RStudio project workspace.
    XML file saved as variable nvd2017Updated.

--- NVD_2017_Updated_RScript.R
+++ NVD_2017_Updated_RScript.R
@@ -1,2 +1,19 @@
 #[1]
 #NVD_2017_Updated_RScript.R
+
+#[2]
+#install.packages("XML")
+#install.packages("methods")
+#install.packages("plyr")
+#install.packages("dplyr")
+#install.packages("anytime")
+
+library(XML)
+library(methods)
+library(plyr)
+library(dplyr)
+library(anytime)
+
+#[3]
+nvd2017Updated <- xmlParse(file = "nvdcve-2.0-2017-updated.xml")
+summary(nvd2017Updated)

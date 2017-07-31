eeacac1b2e62792da1e20577483a8cfef9e858bc
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-31 12:54:09 +0100

    Packages installed through the install.packages() function and imported via the library() function. "recommenderlab".
    set.seed() function applied and set to '1', in order to reproduce the code obtaining the same output.

--- Recommender_System_RScript.R
+++ Recommender_System_RScript.R
@@ -5,3 +5,13 @@
 #https://support.rstudio.com/hc/en-us/community/posts/115000475648-Memory-limit
 memory.limit()
 memory.size(15000)
+
+#[2]
+#R package for recommendation - recommenderlab
+if(!"recommenderlab" %in% rownames(install.packages()))
+{
+	install.packages("recommenderlab")
+}
+library(recommenderlab)
+help(package = "recommenderlab")
+set.seed(1)

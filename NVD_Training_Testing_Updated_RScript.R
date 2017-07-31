b4f4c5d1595207d3dc8cd9ba54d7bf3c7193909a
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-07-31 11:10:06 +0100

    nvd2014UpdatedTotalDistinctRejectRemoved data frame and nvd2015UpdatedTotalDistinctRejectRemoved data frame merged, by use of the merge() function. Data frame nvdMerge2014_2015Updated was created, which combines both data frames: nvd2014UpdatedTotalDistinctRejectRemoved and nvd2015UpdatedTotalDistinctRejectRemoved.

--- NVD_Training_Testing_Updated_RScript.R
+++ NVD_Training_Testing_Updated_RScript.R
@@ -1,1 +1,5 @@
 #NVD_Training_Testing_Updated_RScript.R
+
+#[31]
+#https://stackoverflow.com/questions/37257572/how-can-i-combine-two-dataframes-with-different-lengths-in-r
+nvdMerge2014_2015Updated <- merge(nvd2014UpdatedTotalDistinctRejectRemoved, nvd2015UpdatedTotalDistinctRejectRemoved, all = TRUE)

2a9519742408bf57aa45ef4f5d5ec85745d8c0da
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-08-02 12:40:59 +0100

    Test code added to to NVD_Training_Testing_Updated_RScript.R, with the tag: '#---Test Code [NA]---#'

--- NVD_Training_Testing_Updated_RScript.R
+++ NVD_Training_Testing_Updated_RScript.R
@@ -3,13 +3,20 @@
 #[31]
 #https://stackoverflow.com/questions/37257572/how-can-i-combine-two-dataframes-with-different-lengths-in-r
 nvdMerge2014_2015Updated <- merge(nvd2014UpdatedTotalDistinctRejectRemoved, nvd2015UpdatedTotalDistinctRejectRemoved, all = TRUE)
+#---Test Code [NA]---#
+nvdMerge2014_2015NAUpdated <- merge(nvd2014UpdatedTotalDistinctNARemoved, nvd2015UpdatedTotalDistinctNARemoved, all = TRUE)
 
 #[32]
 nvdMerge2014_2015_2016Updated <- merge(nvdMerge2014_2015Updated, nvd2016UpdatedTotalDistinctRejectRemoved, all = TRUE)
+#---Test Code [NA]---#
+nvdMerge2014_2015_2016NAUpdated <- merge(nvdMerge2014_2015NAUpdated, nvd2016UpdatedTotalDistinctNARemoved, all = TRUE)
 
 #[32]***
 nvdMerge2014_2015_2016_2017Updated <- merge(nvdMerge2014_2015_2016Updated, nvd2017UpdatedTotalDistinctRejectRemoved, all = TRUE)
 nvdMerge2014_2017TotalUpdated <- nvdMerge2014_2015_2016_2017Updated 
+#---Test Code [NA]---#
+nvdMerge2014_2015_2016_2017NAUpdated <- merge(nvdMerge2014_2015_2016NAUpdated, nvd2017UpdatedTotalDistinctNARemoved, all = TRUE)
+nvdMerge2014_2017TotalNAUpdated <- nvdMerge2014_2015_2016_2017NAUpdated
 
 #----------Training----------#
 #[33]

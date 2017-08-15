373fa6791c66e5eab40ac3630ea998464350269e
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-08-15 16:04:35 +0100

    Modified test code added to 'Recommender_System_Model_Build_RScript.R' RScript.

--- Recommender_System_Model_Build_RScript.R
+++ Recommender_System_Model_Build_RScript.R
@@ -45,3 +45,44 @@ getModel(recommender_system_model)
 #dist_test <- as(recommender_system_model@model$sim, "matrix")
 #View(dist_test)
 #summary(dist_test)
+
+
+#
+##
+###
+#Test code - remove if necessary
+#----- Utilised through the 'Recommender_System_Similarity_Matrix_Modified_RScript.R' RScript -----#
+
+#[3]
+trainingModified <- sample(x = c(TRUE, FALSE),
+									 size = nrow(nvd2014_2017ColumnsTotalModifiedMatrixBinary),
+									 replace = TRUE,
+									 prob = c(0.8, 0.2))
+summary(trainingModified)
+21714 + 5320 #TRUE = 21714 / FALSE = 5320
+nvd2014_2017ColumnsTotalModifiedMatrixBinary #27034 x 187 rating matrix of class ‘binaryRatingMatrix’ with 212381 ratings.
+recommender_system_training_data_modified <- nvd2014_2017ColumnsTotalModifiedMatrixBinary[trainingModified, ]
+recommender_system_training_data_modified #21541 x 187 rating matrix of class ‘binaryRatingMatrix’ with 169190 ratings.
+recommender_system_testing_data_modified <- nvd2014_2017ColumnsTotalModifiedMatrixBinary[!trainingModified, ]
+recommender_system_testing_data_modified #5493 x 187 rating matrix of class ‘binaryRatingMatrix’ with 43191 ratings.
+
+#[4]
+recommender_system_model_modified <- Recommender(data = recommender_system_training_data_modified, 
+																				method = "IBCF",
+																				parameter = list(method = "Jaccard"))
+recommender_system_model_modified
+
+class(recommender_system_model_modified@model$sim)
+dim(recommender_system_model_modified@model$sim)
+image(recommender_system_model_modified@model$sim)
+range(recommender_system_model_modified@model$sim)
+getModel(recommender_system_model_modified)
+
+#[Test Modified]
+#dist_test_modified <- as(recommender_system_model_modified@model$sim, "matrix")
+#View(dist_test_modified)
+#summary(dist_test_modified)
+
+###
+##
+#

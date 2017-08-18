5c619fde6b5eaab77ecb3fa7013f92399b97c7e9
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-08-18 16:07:54 +0100

    ARFF (Attribute-Relation File Format) Conversion, from Data Frame to ARFF.
    nvd_2014_2017_total_columns_ordered.arff file created and added to project workspace.

--- NVD_ARFF_Conversion.R
+++ NVD_ARFF_Conversion.R
@@ -1,2 +1,11 @@
 #NVD_ARFF_Conversion.R
 
+#----------ARFF (Attribute-Relation File Format) Conversion, from Data Frame to ARFF----------#
+#https://stackoverflow.com/questions/40880549/how-to-write-data-frame-into-arff-using-r
+
+install.packages("foreign")
+library(foreign)
+
+#Data Frame: "nvd2014_2017SelectedColumnsModifiedOrdered"
+#Data Frame RScript File Location: "Recommender_System_Similarity_Matrix_Modified_RScript.R"
+write.arff(nvd2014_2017SelectedColumnsModifiedOrdered, file = "nvd_2014_2017_total_columns_ordered.arff")
